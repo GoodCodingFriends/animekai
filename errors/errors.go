0ec6b2eeede8ec8749a5016a833070122ecd667e
ktr0731 <ktr@syfm.me>
2020-05-17 20:19:40 +0900

    define some errors

---
+++ errors/errors.go
@@ -0,0 +1,9 @@
+package errors
+
+import "github.com/morikuni/failure"
+
+const (
+	Canceled         failure.StringCode = "Canceled"
+	DeadlineExceeded failure.StringCode = "DeadlineExceeded"
+	Internal         failure.StringCode = "Internal"
+)
